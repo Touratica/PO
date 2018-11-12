ba43de68584e52eb954dfd112c0b782b5d9a05de
João Pedro Almeida <joao.henriques.almeida@tecnico.ulisboa.pt>
2018-11-12 23:50:44 +0000

    Minor changes to School class
    	* Creates input stream
    	* Creates switch for checking the person's function on the school

--- Projeto/project/sth-core/src/sth/School.java
+++ Projeto/project/sth-core/src/sth/School.java
@@ -2,6 +2,11 @@ package sth;
 
 //FIXME import other classes if needed
 
+import sth.io.Serializable;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
 import sth.exceptions.BadEntryException;
 import sth.exceptions.InvalidCourseSelectionException;
 import sth.exceptions.NoSuchPersonIdException;
@@ -11,22 +16,47 @@ import sth.exceptions.NoSuchPersonIdException;
  */
 public class School implements Serializable {
 
-  /** Serial number for serialization. */
-  private static final long serialVersionUID = 201810051538L;
+	/** Serial number for serialization. */
+	private static final long serialVersionUID = 201810051538L;
 
-  //FIXME define object fields (attributes and, possibly, associations)
+	//FIXME define object fields (attributes and, possibly, associations)
 
-  //FIXME implement constructors if needed
-  
-  /**
-   * @param filename
-   * @throws BadEntryException
-   * @throws IOException
-   */
-  void importFile(String filename) throws IOException, BadEntryException {
-    //FIXME implement text file reader
-  }
-  
-  //FIXME implement other methods
+	//FIXME implement constructors if needed
+	
+	/**
+	 * @param filename
+	 * @throws BadEntryException
+	 * @throws IOException
+	 */
+	void importFile(String filename) throws IOException, BadEntryException {
+		//FIXME implement text file reader
+		DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(filename)));
+		String function;
+		
+		switch (function = in.readUTF()) {
+			case "FUNCIONÁRIO":
+				
+				break;
+		
+			case "DELEGADO":
+
+				break;
+
+			case "ALUNO":
+
+				break;
+
+			case "DOCENTE":
+
+				break;
+
+			default:
+				throw new BadEntryException(entrySpecification);
+				break;
+		}
+
+	}
+	
+	//FIXME implement other methods
 
 }
