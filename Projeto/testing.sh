f72d23cf142e10e34e7c4e404f2b3552ee461055
João Pedro Almeida <joao.henriques.almeida@tecnico.ulisboa.pt>
2018-12-06 03:49:41 +0000

    Minor fix to testing script

--- Projeto/testing.sh
+++ Projeto/testing.sh
@@ -5,8 +5,8 @@ for test in Tests-ef-daily-201812032309/auto-tests/*.in
 do
 	test=${test##*/}
 	test=${test%.in}
-	echo -e "\033[1;32mTest: $test\033[0m"
-	(cd project && java -Dimport="../Tests-ef-daily-201812032309/auto-tests/${test##*/}.import" -Din="../Tests-ef-daily-201812032309/auto-tests/$test.in" -Dout="../Tests-ef-daily-201812032309/auto-tests/$test.outhyp" sth.app.App && diff -s -b "../Tests-ef-daily-201812032309/auto-tests/$test.outhyp" "../Tests-ef-daily-201812032309/auto-tests/expected/$test.out" > ../tmp.txt)
+	echo -e "\033[1;32mTest: ${test}\033[0m"
+	(cd project && java -Dimport="../Tests-ef-daily-201812032309/auto-tests/${test}.import" -Din="../Tests-ef-daily-201812032309/auto-tests/${test}.in" -Dout="../Tests-ef-daily-201812032309/auto-tests/${test}.outhyp" sth.app.App && diff -s -b "../Tests-ef-daily-201812032309/auto-tests/${test}.outhyp" "../Tests-ef-daily-201812032309/auto-tests/expected/${test}.out" > ../tmp.txt)
 	if [ $? -eq 0 ]; then
 		echo -e "\033[1;31mThe files are identical\033[0m"
 	else
